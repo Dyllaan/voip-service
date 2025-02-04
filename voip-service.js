8cb29b885640471d4ccdd586fd929ce3a696e310
Louis <38404111+Dyllaan@users.noreply.github.com>
2025-02-04 00:19:31 +0000

    first

---
+++ voip-service.js
@@ -0,0 +1,17 @@
+const { PeerServer } = require("peer");
+
+const PORT = process.env.PORT || 9002;
+
+const peerServer = PeerServer({
+  port: PORT,
+  path: "/peerjs",
+  allow_discovery: true,
+  corsOptions: {
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }
+});
+
+console.log(`Running on port ${PORT}`);
